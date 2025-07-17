25072281d900e67de34d130eba2feb7337c04e00
Deepak Shukla <flamesofwisdom004@gmail.com>
2025-07-17 23:31:16 +0530

    fix: add health check route for Render

--- server.js
+++ server.js
@@ -20,3 +20,7 @@ mongoose.connect(process.env.MONGO_URI, {
   console.log("MongoDB Connected");
   app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
 }).catch(err => console.error(err));
+
+app.get('/', (req, res) => {
+  res.send('Leaderboard API is running');
+});
